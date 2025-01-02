ec104595c94ec331456cae1e0b3ce2bd41cf9bb6
waizin28 <waizinlinn.08@gmail.com>
2025-01-02 10:02:18 -0600

    first commit

---
+++ LOTRConverter/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LOTRConverter
+//
+//  Created by Wai Zin Linn on 1/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+                .foregroundColor(Color.purple)
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Wai Zin Linn on 1/2/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
