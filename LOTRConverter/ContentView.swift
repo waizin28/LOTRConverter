a06bab4aae28e190cf9d1b3a3e7c734ef2c5f9b3
waizin28 <waizinlinn.08@gmail.com>
2025-01-02 10:48:35 -0600

    added sf symbol, images, and text

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,12 +11,22 @@ struct ContentView: View {
     var body: some View {
         ZStack{
             // Background Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             // Hold Everything else
             VStack{
                 // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency Exchange Text (Title)
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // Conversion Section
                 HStack{
@@ -24,30 +34,57 @@ struct ContentView: View {
                     VStack{
                         // Currency
                         HStack{
+                            
                             // Currency Image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             // Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // Text Field
+                        Text("Text Field")
                     }
                     
                     // Equal Sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right conversion section
                     VStack{
                         // Currency
                         HStack{
                             // Currency Text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Currency Image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // Text Field
+                        Text("Text Field")
                     }
                 }
                 
-                // Conversion Info
+                Spacer()
+                
+                // Conversion Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    
             }
         }
     }
