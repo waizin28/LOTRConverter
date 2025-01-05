a815282fe78f73bfea507d1b871d5b14b3d9c2b7
waizin28 <waizinlinn.08@gmail.com>
2025-01-05 12:58:16 -0600

    created model for currency

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Wai Zin Linn on 1/5/25.
+//
+
+enum Currency: Double {
+    case copperPenny = 6400
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
