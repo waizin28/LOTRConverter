82311165359f68bfcc71f1d083cc14d2bd903d24
waizin28 <waizinlinn.08@gmail.com>
2025-01-05 13:14:56 -0600

    defined compute resource to determine corresponding image and text

--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -5,10 +5,44 @@
 //  Created by Wai Zin Linn on 1/5/25.
 //
 
+import SwiftUI
+
 enum Currency: Double {
     case copperPenny = 6400
     case silverPenny = 64
     case silverPiece = 16
     case goldPenny = 4
     case goldPiece = 1
+    
+    // computed property -> compute every single time
+    var image: ImageResource {
+        switch self{
+            case .copperPenny:
+                    .copperpenny
+            case .silverPenny:
+                    .silverpenny
+            case .silverPiece:
+                    .silverpiece
+            case .goldPenny:
+                    .goldpenny
+            case .goldPiece:
+                    .goldpiece
+        }
+            
+    }
+    
+    var name: String {
+        switch self {
+            case .copperPenny:
+                "Copper Penny"
+            case .silverPenny:
+                "Silver Penny"
+            case .silverPiece:
+                "Silver Piece"
+            case .goldPenny:
+                "Gold Penny"
+            case .goldPiece:
+                "Gold Piece"
+        }
+    }
 }
